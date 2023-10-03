746e8531cb9792fc072b3b4a09da4cee91250d7b
Riya <riyabansal2103@gmail.com>
2023-10-04 04:49:15 +0530

    updated hero,features,vedio

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -15,23 +15,25 @@ const Hero = () => {
                 data-wow-delay=".2s"
               >
                 <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
-                Free and Open-Source Next.js Template for Startup & SaaS
+                Software Freedom Day 2023!!
                 </h1>
                 <p className="mb-12 text-base font-medium !leading-relaxed text-body-color dark:text-white dark:opacity-90 sm:text-lg md:text-xl">
-                Startup is free Next.js template for startups and SaaS business websites comes with all the essential pages, components, and sections you need to launch a complete business website, built-with Next 13.x and Tailwind CSS.
+                Software Freedom Day is an event held all across the world to celebrate the usage of Free and Open Source Software. The event aims to sensitize students about the virtues of Open Source Software. Each year the event has seen growing number of participants and our past speakers have come from prestigious software companies like Facebook, Microsoft and Google.<br/><br/>
+
+                The aim of SFD is to bring together developers and students of all levels on a single platform to share their thoughts, ideas and inspirations. This year, Programming Club of UIET brings to you the 12th Edition of Software Freedom Day on September 17, 2022 & September 18, 2022.
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
+                    href="https://www.softwarefreedomday.org/"
                     className="rounded-md bg-primary py-4 px-8 text-base font-semibold text-white duration-300 ease-in-out hover:bg-primary/80"
                   >
-                   🔥 Get Pro
+                   Read More 
                   </Link>
                   <Link
-                    href="https://github.com/NextJSTemplates/startup-nextjs"
+                    href="https://docs.google.com/forms/d/e/1FAIpQLSdmKt-lSHqe-54tYLC0DkQTjhtlKBVBCJymOb9z9GPqCF90SA/viewform"
                     className="rounded-md bg-black/20 py-4 px-8 text-base font-semibold text-black duration-300 ease-in-out hover:bg-black/30 dark:bg-white/20 dark:text-white dark:hover:bg-white/30"
                   >
-                    Star on GitHub
+                    Register Here
                   </Link>
                 </div>
               </div>
